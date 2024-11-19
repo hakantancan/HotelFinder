d66aba6dbbaba204b95fc7cdc20dbbc7e9773fd2
Hakan Tancan <hakantancan09@gmail.com>
2024-11-19 18:10:41 +0300

    Add project files.

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HotelDb;");  //Oluşturulacak veritabanı yeri ve ismi
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }   //Hotel veritabanında oluşturulacak tablonun ismi Hotels
+    }
+}
